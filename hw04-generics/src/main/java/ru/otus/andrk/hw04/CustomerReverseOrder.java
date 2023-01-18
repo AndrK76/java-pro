d049b09de309f73e4190b580a039d97dcf49250a
AndrK76 <andrk-zlt@74.ru>
2023-01-18 10:38:09 +0500

    HW begin

---
+++ hw04-generics/src/main/java/ru/otus/andrk/hw04/CustomerReverseOrder.java
@@ -0,0 +1,18 @@
+package ru.otus.andrk.hw04;
+
+
+import ru.otus.andrk.hw04.Customer;
+
+public class CustomerReverseOrder {
+
+    //todo: 2. надо реализовать методы этого класса
+    //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
+
+    public void add(Customer customer) {
+
+    }
+
+    public Customer take() {
+        return null; // это "заглушка, чтобы скомилировать"
+    }
+}
