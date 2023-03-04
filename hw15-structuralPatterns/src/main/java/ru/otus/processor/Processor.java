def54451afd9691addeba2bb0e5d8ed863cbb0e3
AndrK76 <andrk-zlt@74.ru>
2023-03-04 21:32:41 +0500

    hw15 todo2

--- hw15-structuralPatterns/src/main/java/ru/otus/processor/Processor.java
+++ hw15-structuralPatterns/src/main/java/ru/otus/processor/Processor.java
@@ -6,7 +6,8 @@ public interface Processor {
 
     Message process(Message message);
 
-    //todo: 2. Сделать процессор, который поменяет местами значения field11 и field12
+    //todo: 2. DONE Сделать процессор, который поменяет местами значения field11 и field12
+    //реализация: ProcessorSwitchFields, тесты: ProcessorSwitchFieldsTest
 
     //todo: 3. Сделать процессор, который будет выбрасывать исключение в четную секунду (сделайте тест с гарантированным результатом)
     //         Секунда должна определяьться во время выполнения.
---
+++ hw15-structuralPatterns/src/main/java/ru/otus/processor/ProcessorSwitchFields.java
@@ -0,0 +1,13 @@
+package ru.otus.processor;
+
+import ru.otus.model.Message;
+
+public class ProcessorSwitchFields implements Processor{
+    @Override
+    public Message process(Message message) {
+        String field11old = message.getField11();
+        String field12old = message.getField12();
+        Message ret = message.toBuilder().field11(field12old).field12(field11old).build();
+        return ret;
+    }
+}
